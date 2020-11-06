a104106398a168cff107ceb5ec314852fa90f671
Garande <hassaniwafula@gmail.com>
2020-11-07 00:55:57 +0300

    Initial commit

---
+++ app/src/main/java/com/yellowbird/g_vpn/view/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yellowbird.g_vpn.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yellowbird.g_vpn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
